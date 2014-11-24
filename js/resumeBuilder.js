9091b22298cdf3b98ec317ed5ea572200e3586fc
Erik Price <erikprice13@gmail.com>
2014-11-24 14:26:27 -0800

    Convert files after lesson 1 for lesson 3. Includes variable changes to .js files

---
+++ js/resumeBuilder.js
@@ -0,0 +1,32 @@
+
+var skillsArray = ["HTML", "CSS", "Javascript", "Java", "C++"];
+var bio = {
+	"myName" : "Erik Price",
+	"myRole" : "Web Designer",
+	"contacts" : {
+		"mobile" : "253-376-6876",
+		"email" : "erikprice13@gmail.com",
+		"github" : "github.com/erikprice13",
+		"location" : "Gig Harbor"
+	},
+	"welcomeMessage" : "Welcome to my resume page.",
+	"skills" : skillsArray,
+	"myPicture" : "images/me.png"
+};
+
+var work = {};
+work.currentPosition = "Receptionist";
+work.employer = "Great Clips";
+work.yearsWorked = "1";
+work.city ="Port Orchard";
+
+var education = {};
+education["nameOfSchool"] = "Tacoma Community College";
+education["yearsAttended"] = "3";
+education["cityOfSchool"] = "Tacoma";
+
+var projects = {
+	"githubContribution" : "Placeholder",
+	"websiteMockup" : "Placeholder",
+	"thisResume" : "Placeholder"
+};
