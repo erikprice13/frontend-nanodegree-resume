191dcebdf8b2046c06502d1703526c4aa7ff8812
Erik Price <erikprice13@gmail.com>
2014-12-03 10:39:29 -0800

    Create 4 main information objects and form header, work and skills sections.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,3 @@
-
-var skillsArray = ["HTML", "CSS", "Javascript", "Java", "C++"];
 var bio = {
 	"myName" : "Erik Price",
 	"myRole" : "Web Designer",
@@ -10,23 +8,66 @@ var bio = {
 		"location" : "Gig Harbor"
 	},
 	"welcomeMessage" : "Welcome to my resume page.",
-	"skills" : skillsArray,
+	"skills" : ["HTML", "CSS", "Javascript", "Java", "C++"],
 	"myPicture" : "images/me.png"
 };
 
-var work = {};
-work.currentPosition = "Receptionist";
-work.employer = "Great Clips";
-work.yearsWorked = "1";
-work.city ="Port Orchard";
+var work = {
+	"jobs" : {
+		"Miracle Ranch" : {
+			"currentPosition" : "Server",
+			"employer" : "CHRISTA Camps",
+			"yearsWorked" : "3",
+			"city" : "Port Orchard",
+			"dates" : "Aug 2011 to Present",
+			"myDescription" : "Deep cleaning, serving food, constant public contact, leadership, time management, management skills."
+		},
+		"Great Clips" : {
+			"currentPosition" : "Receptionist",
+			"employer" : "Great Clips",
+			"yearsWorked" : "1",
+			"city" : "Port Orchard",
+			"dates" : "Sept 2014 to Present",
+			"myDescription" : "Sweeping, counting and running money, manning the front desk, talking with customers, memorizing hundreds of products and what they do, running all background jobs and chores to run a hair salon."
+		}
+	}
+};
 
-var education = {};
-education["nameOfSchool"] = "Tacoma Community College";
-education["yearsAttended"] = "3";
-education["cityOfSchool"] = "Tacoma";
+var education = {
+	"nameOfSchool" : "Tacoma Community College",
+	"yearsAttended" : "3",
+	"cityOfSchool" : "Tacoma"
+};
 
 var projects = {
 	"githubContribution" : "Placeholder",
 	"websiteMockup" : "Placeholder",
 	"thisResume" : "Placeholder"
 };
+
+var formattedName = HTMLheaderName.replace("%data%", bio.myName);
+$("#header").append(formattedName);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.myRole);
+$("#header").append(formattedRole);
+var formattedPic = HTMLbioPic.replace("%data%", bio.myPicture);
+$("#header").append(formattedPic);
+
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+	for (var i = 0; i < bio.skills.length; i++) {
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+		$("#skills").append(formattedSkill);
+	}
+}
+
+for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].currentPosition);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].myDescription);
+	$(".work-entry:last").append(formattedDescription);
+}
